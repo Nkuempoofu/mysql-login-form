aec0bf3e9a86a78ba5a58603584fcd4c77efee00
NKULULEKO MPOFU <66128630+Nkuempoofu@users.noreply.github.com>
2020-06-25 13:02:29 +0200

    Add files via upload
    c# mysql Login form

---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        MySqlConnection con = new MySqlConnection(@"Data Source=localhost;port=3306;Initial
+               Catalog=test2;User Id=root;password=''");
+
+
+        int i;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            i = 0;
+            con.Open();
+            MySqlCommand cmd = con.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "select * from login where username= '" + textBox1.Text + "' and password '" + textBox2.Text + "'";
+            cmd.ExecuteNonQuery();
+            DataTable dt = new DataTable();
+            MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+            da.Fill(dt);
+            i = Convert.ToInt32(dt.Rows.Count.ToString());
+
+            if (i == 0)
+            {
+                label3.Text = " The username or password you entered is invalid.";
+            }
+
+            else
+            {
+                this.Hide();
+                Form2 fm = new Form2();
+                fm.Show();
+            }
+
+            con.Close();
+        }
+    }
+}
